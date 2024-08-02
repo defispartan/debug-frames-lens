cafcb90c564e437de18f01feec2495c879a2b513
defispartan <defispartan@gmail.com>
2024-08-01 23:50:06 -0500

    feat: anonymous txn frame

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,33 +1,33 @@
 /* eslint-disable react/jsx-key */
-import {farcasterHubContext, openframes} from "frames.js/middleware";
-import {imagesWorkerMiddleware} from "frames.js/middleware/images-worker";
-import {createFrames} from "frames.js/next";
-import {DEFAULT_DEBUGGER_HUB_URL} from "../debug";
-import {getLensFrameMessage, isLensFrameActionPayload} from "frames.js/lens";
+import { openframes } from "frames.js/middleware";
+import { imagesWorkerMiddleware } from "frames.js/middleware/images-worker";
+import { createFrames } from "frames.js/next";
+import { JsonValue } from "frames.js/types";
+
+interface MessageWithWalletAddressImplementation {
+  walletAddress: () => Promise<string | undefined>;
+}
 
 export const frames = createFrames({
-    basePath: "/frames",
-    initialState: {
-        pageIndex: 0,
-    },
-    middleware: [
-        imagesWorkerMiddleware({
-            imagesRoute: "/images",
-        }),
-        openframes({
-            clientProtocol: {
-                id: "lens",
-                version: "1.0.0",
-            },
-            handler: {
-                isValidPayload: (body: JSON) => isLensFrameActionPayload(body),
-                getFrameMessage: async (body: JSON) => {
-                    if (!isLensFrameActionPayload(body)) {
-                        return undefined;
-                    }
-                    return {...await getLensFrameMessage(body)};
-                },
-            },
-        }),
-    ],
+  basePath: "/frames",
+  initialState: {
+    pageIndex: 0,
+  },
+  middleware: [
+    imagesWorkerMiddleware({
+      imagesRoute: "/images",
+    }),
+    openframes({
+      clientProtocol: {
+        id: "anonymous",
+        version: "1.0.0",
+      },
+      handler: {
+        isValidPayload: (body: JsonValue) => true,
+        getFrameMessage: async (body: JsonValue) => {
+          return body as unknown as MessageWithWalletAddressImplementation;
+        },
+      },
+    }),
+  ],
 });
--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -1,23 +1,42 @@
 /* eslint-disable react/jsx-key */
-import {Button} from "frames.js/next";
-import {frames} from "./frames";
+import { Button } from "frames.js/next";
+import { frames } from "./frames";
 
 const handleRequest = frames(async (ctx) => {
+  if (ctx.message?.untrustedData?.transactionId) {
     return {
-        image: (
-            <div tw="flex flex-col">
-                <div tw="flex">Click the button</div>
-            </div>
-        ),
-        buttons: [
-            <Button action="post" target="/who-am-i">
-                Who am I?
-            </Button>,
-            <Button action="tx" target="/tx/approve" post_url="/">
-                Approve
-            </Button>,
-        ],
+      image: (
+        <div tw="flex flex-col">
+          <div tw="flex">
+            Tx Hash: {ctx.message.untrustedData.transactionId}
+          </div>
+        </div>
+      ),
+      buttons: [
+        <Button action="post" target="/who-am-i">
+          Who am I?
+        </Button>,
+        <Button action="tx" target="/tx/approve" post_url="/">
+          Approve
+        </Button>,
+      ],
     };
+  }
+  return {
+    image: (
+      <div tw="flex flex-col">
+        <div tw="flex">Click the button</div>
+      </div>
+    ),
+    buttons: [
+      <Button action="post" target="/who-am-i">
+        Who am I?
+      </Button>,
+      <Button action="tx" target="/tx/approve" post_url="/">
+        Approve
+      </Button>,
+    ],
+  };
 });
 
 export const GET = handleRequest;
