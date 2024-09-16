34d1ceeaece44453699300cfd5c78f0626afd010
defispartan <defispartan@gmail.com>
2024-09-16 11:40:51 -0600

    feat: amoy test tx (#1)

--- app/frames/tx/approve/route.tsx
+++ app/frames/tx/approve/route.tsx
@@ -1,25 +1,17 @@
-import {frames} from "../../frames";
-import {ERC20abi} from "../../../lib/definitions/abi/erc20-abi";
-import {Abi, encodeFunctionData} from "viem";
-import {transaction} from "frames.js/core";
-import {ethers} from "ethers";
-import {EthSendTransactionParams} from "frames.js";
+import { frames } from "../../frames";
+import { transaction } from "frames.js/core";
+import { Abi } from "viem";
+import { EthSendTransactionParams } from "frames.js";
 
 export const POST = frames(async (ctx) => {
-    return transaction({
-        chainId: `eip155:8453`,
-        method: "eth_sendTransaction",
-        params: {
-            abi: ERC20abi as Abi,
-            to: "0x4ed4e862860bed51a9570b96d89af5e1b0efefed",
-            data: encodeFunctionData({
-                abi: ERC20abi,
-                functionName: "approve",
-                args: [
-                    "0x948092958c66f228e6C7afcc96935661eea4a3c5",
-                    ethers.parseUnits("1", 18)
-                ]
-            })
-        } as EthSendTransactionParams,
-    });
+  return transaction({
+    chainId: `eip155:80002`,
+    method: "eth_sendTransaction",
+    params: {
+      abi: [] as Abi,
+      to: "0x0000000000000000000000000000000000000000",
+      value: "0x0",
+      data: "0x",
+    } as EthSendTransactionParams,
+  });
 });
